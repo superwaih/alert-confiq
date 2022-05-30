b8caad34f906200e50e9b08c0486bdaa7d3ef9c5
shittu adewale <sheriffhardewale@gmail.com>
2022-05-30 07:53:21 +0100

    remove some console log

--- src/App.js
+++ src/App.js
@@ -3,25 +3,6 @@ import { Table } from "./component/Table";
 
 
 function App() {
-  const[row, setRow] = useState([])
-  const data = [{
-    name: "alert1",
-    independent_var: ['ME Torge (Avg).', 'ME Speed (Avg).'],
-    dependent_var: ['ME Speed (Avg.)'],
-    freg: 10
-  }, {
-    name: "alert1",
-    independent_var: ['ME Torge (Avg).', 'ME Speed (Avg).'],
-    dependent_var: ['ME Speed (Avg.)'],
-    freg: 10
-  }]
-
- 
-  const arr = ['alert1', 'alert2']
-
-  console.log(arr)
-  useEffect(() =>{
-  }, [])
  
   return (
     <div className="App">
